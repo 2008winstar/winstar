1b721b434dcc3c71afda711563857a71400cca30
2008winstar <2008winstar@sina.com>
2016-09-14 23:36:13 +0800

    commit redis demo

--- algorithm/insertionSort.js
+++ algorithm/insertionSort.js
@@ -1,18 +1,20 @@
 /**
- * @file file description here.
+ * @file 插入排序算法示例
  * @author winstar
  * @date 2016/9/14
  */
-
+'use strict';
 let arr = [5, 2, 4, 6, 1, 3];
 let sort = [arr[0]];
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < sort.length; j++) {
-        let outerItem = arr[i];
-        let innerItem = sort[j];
-        if (outerItem >= innerItem) {
-            sort.push(outerItem);
-        }
+
+for (let i = 1; i < arr.length; i++) {
+    let outerItem = arr[i];
+    let j = sort.length  - 1;
+    while (sort[j] && sort[j] > outerItem) {
+        sort[j + 1] = sort[j];
+        j--
     }
+    sort[j + 1] = outerItem;
 }
+
 console.log(sort);
